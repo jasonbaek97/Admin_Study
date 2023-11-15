bb57fe89b55d7f2af5121610a2c039169b6f2077
Jason <ssennom0@naver.com>
2023-11-15 12:09:31 +0900

    lombok

--- src/main/java/com/example/admin_study/model/SearchParam.java
+++ src/main/java/com/example/admin_study/model/SearchParam.java
@@ -1,5 +1,10 @@
 package com.example.admin_study.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class SearchParam {
     private String account;
     private String email;
@@ -7,27 +12,27 @@ public class SearchParam {
 
     // { "account" : "", "email" : "", "page": 0 }
 
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
+//    public String getAccount() {
+//        return account;
+//    }
+//
+//    public void setAccount(String account) {
+//        this.account = account;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public int getPage() {
+//        return page;
+//    }
+//
+//    public void setPage(int page) {
+//        this.page = page;
+//    }
 }
